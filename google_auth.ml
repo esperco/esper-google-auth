614b7fcfa9224e97bea94d55708ebde58fd28c59
Martin Jambon <martin@mjambon.com>
2014-05-23 18:48:28 -0700

    Progress on Google sign-in. Now we need to pass a secret from the client to the server in order to validate the login.

--- google_auth.ml
+++ google_auth.ml
@@ -7,7 +7,7 @@ let client_secret = "dJjpTdtAouZq8UNzm1q0csbG"
 (*
    List of permissions requested
 *)
-let scopes = String.concat " " (List.sort String.compare [
+let scopes = String.concat " " [
   (*
      Sign-in (this scope includes the profile scope)
      https://developers.google.com/+/api/oauth#plus.login
@@ -30,21 +30,46 @@ let scopes = String.concat " " (List.sort String.compare [
 
   (* Calendar *)
   "https://www.googleapis.com/auth/calendar";
-])
+]
 
-let auth_uri state =
+let auth_uri ?login_hint ~request_new_refresh_token state =
+  let approval_prompt =
+    if request_new_refresh_token then
+      (*
+        This results in a new refresh_token,
+        however the user will be prompted for "offline access"
+        even if they already gave their permission for all the scopes
+        for the previous token.
+
+        In order to not prompt the user each time they click on the
+        "Sign in with Google" button, one must not request a new refresh_token.
+      *)
+      "force"
+    else
+      (*
+        Prompt the user for missing permissions if any, but don't produce
+        a new refresh_token.
+      *)
+      "auto"
+  in
+  let login_hint =
+    match login_hint with
+    | None -> []
+    | Some email -> [ "login_hint", [Email.to_string email] ]
+  in
   Uri.make
     ~scheme:"https"
     ~host:"accounts.google.com"
     ~path:"/o/oauth2/auth"
-    ~query:["response_type", ["code"];
-            "client_id", [client_id];
-            "redirect_uri", [App_path.google_oauth_callback_url ()];
-            "scope", [scopes];
-            "state", [Google_api_j.string_of_state state];
-            "access_type", ["offline"];
-            "approval_prompt", ["force"];
-           ]
+    ~query: ([
+      "response_type", ["code"];
+      "client_id", [client_id];
+      "redirect_uri", [App_path.google_oauth_callback_url ()];
+      "scope", [scopes];
+      "state", [Google_api_j.string_of_state state];
+      "access_type", ["offline"];
+      "approval_prompt", [approval_prompt];
+    ] @ login_hint)
     ()
 
 let oauth_token_uri () =
@@ -60,7 +85,10 @@ let form_headers =
 let auth_header access_token =
   "Authorization", "Bearer " ^ access_token
 
-type ('a,'b)result = Good of 'a | Bad of 'b
+type token_result =
+  | All_tokens of Account_t.google_oauth_token
+  | Only_access_token of string
+  | Error of string
 
 let oauth_revoke access_token =
   Util_http_client.get (oauth_revoke_uri access_token) >>= function
@@ -72,6 +100,10 @@ let oauth_revoke access_token =
       logf `Warning "Google token revocation failed: %s" body;
       return false
 
+(*
+  Get a refresh_token (i.e. a permanent access token) in exchange for
+  a one-time code posted by Google.
+*)
 let get_token code redirect_uri =
   let q = ["code",          [code];
            "client_id",     [client_id];
@@ -82,21 +114,45 @@ let get_token code redirect_uri =
   Util_http_client.post ~headers:form_headers ~body (oauth_token_uri ())
   >>= fun (_status, _headers, body) ->
   match Google_api_j.oauth_token_result_of_string body with
-    | {Google_api_t.error = Some error} ->
-      return (Bad error)
+    | { Google_api_t.error = Some error } ->
+        return (Error error)
 
-    | {Google_api_t.refresh_token = Some refresh_token; error = None;
-       access_token; expires_in} ->
-      let expiration = Unix.time () +. BatOption.default 0. expires_in in
-      return (Good {Account_t.access_token; refresh_token; expiration})
+    | { Google_api_t.refresh_token = Some refresh_token;
+        access_token;
+        expires_in } ->
+        let expiration = Unix.time () +. BatOption.default 0. expires_in in
+        return (All_tokens {Account_t.access_token; refresh_token; expiration})
 
-    | {Google_api_t.refresh_token = None; error = None} ->
-      return (Bad "no refresh token")
+    | { Google_api_t.refresh_token = None;
+        access_token = Some access_token; } ->
+        return (Only_access_token access_token)
 
+    | _ ->
+        return (Error "missing tokens")
+
+(*
+  Get a valid access_token from a refresh_token, fetching and storing
+  a new one if the last access_token has expired.
+*)
 let refresh token =
+  (*
+     The grace period is how early before the expiration it is acceptable
+     to request a new access token. This is not documented by Google,
+     we are just guessing. The problem is that Google refuses to produce
+     a new access token if the last issued access token is still valid.
+
+     It requires our clocks to be synchronized with
+     each other to that precision and it must leave enough time
+     to perform the subsequent api request to Google using the access token
+     that may be almost expired.
+
+     A better approach might be "try api call, refresh token if needed, retry"
+     as described here: http://stackoverflow.com/a/22810510/597517
+  *)
+  let grace_period = 60. in
   match token with
     | {Account_t.access_token = Some _ as access_token; expiration}
-        when Unix.time () +. 600. <= expiration ->
+        when Unix.time () +. grace_period <= expiration ->
         return (None, access_token)
     | {Account_t.refresh_token} ->
         logf `Info "refresh: need to refresh";
